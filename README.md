3feb06581e631a594b5d194b4ff8cc3252bf68f6
BuildTools <unconfigured@null.spigotmc.org>
2023-07-12 22:34:40 +0200

    Ajout d'un readme et modification des ports du app.js

---
+++ README.md
@@ -0,0 +1,5 @@
+# LeQuizz
+
+Avoir node d'installé
+Lancement : - "npm start" via CMD
+            - Ouvrir la page index.html
--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors({
 
 app.use(function (req, res, next) {
 
-res.header('Access-Control-Allow-Origin', "http://127.0.0.1:5500");
+res.header('Access-Control-Allow-Origin', "http://127.0.0.1");
 res.header('Access-Control-Allow-Headers', true);
 res.header('Access-Control-Allow-Credentials', true);
 res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
